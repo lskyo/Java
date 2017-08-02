65c9dddfae0b3d955739e830ac34abfd3add856e
lskyo <609911762@qq.com>
2017-08-02 21:28:25 +0800

    add MyBatis-02-config
    MyBatis配置文件

---
+++ MyBatis/MyBatis-02-config/src/com/lskyo/mybatis/bean/Employee.java
@@ -0,0 +1,44 @@
+package com.lskyo.mybatis.bean;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("emp")
+public class Employee {
+	private Integer id;
+	private String lastName;
+	private String email;
+	private String gender;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", lastName=" + lastName + ", email=" + email + ", gender=" + gender + "]";
+	}
+	
+	
+	
+	
+	
+}
